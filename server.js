2cfadf835adb5d4aa1b6e4851c26b3f0d048392a
heytosins <qfurkant@gmail.com>
2026-07-04 02:07:32 +0300

    adadada

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: smtpPort,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
